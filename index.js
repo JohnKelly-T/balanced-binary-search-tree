82d7241d41bf475b8a6f8a9cbfcc0e63e62d12fe
john-kelly <johnkellytemena@gmail.com>
2025-05-22 18:43:53 +0800

    Test insert method

--- index.js
+++ index.js
@@ -16,5 +16,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 let array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 let myTree = new Tree(array);
+myTree.insert(10);
+myTree.insert(24);
+myTree.insert(68);
+myTree.insert(350);
 
 prettyPrint(myTree.root);
