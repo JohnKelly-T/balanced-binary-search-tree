8df970c7e6e43b35f4a16777a7c412156662d94d
john-kelly <johnkellytemena@gmail.com>
2025-05-23 20:19:43 +0800

    Add delete item method

--- modules/tree.js
+++ modules/tree.js
@@ -61,4 +61,74 @@ export class Tree {
       }
     }
   }
+
+  deleteItem(value) {
+    let parent = this.root;
+    let target = this.root;
+
+    // traverse to target node
+    while (target !== null) {
+      if (target.data === value) break;
+
+      parent = target;
+      target = (value < target.data) ? target.left : target.right;
+    }
+
+    if (target === null) throw new Error("Value not in tree");
+
+    // case 1: leaf node
+    if (target.left === null && target.right === null) {
+      if (target.data < parent.data) {
+        parent.left = null;
+      } else if (target.data > parent.data){
+        parent.right = null;
+      } else if (target === this.root) {
+        this.root = null;
+      }
+
+      return;
+    }
+
+    // case 2: node with only one child
+    if (target.left === null || target.right === null) {
+      let targetChild = target.left === null ? target.right : target.left;
+
+      if (target.data < parent.data) {
+        parent.left = targetChild;
+      } else if (target.data > parent.data) {
+        parent.right = targetChild;
+      } else if (target === this.root) {
+        this.root = targetChild;
+      }
+      
+      return;
+    } 
+
+    // case 3: node with two children 
+    let replacementParent = target;
+    let replacementChild = target.right;
+
+
+    // find the next largest node
+    while (replacementChild.left !== null) {
+      replacementParent = replacementChild;
+      replacementChild = replacementChild.left;
+    }
+
+    // move replacement to target
+    replacementChild.left = target.left;
+
+    if (replacementParent !== target) {
+      replacementParent.left = replacementChild.right;
+      replacementChild.right = target.right;
+    }
+    
+    if (target.data < parent.data) {
+      parent.left = replacementChild;
+    } else if (target.data > parent.data) {
+      parent.right = replacementChild;
+    } else if (target === this.root) {
+      this.root = replacementChild;
+    }
+  }
 }
